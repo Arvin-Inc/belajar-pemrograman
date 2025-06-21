676a6b99ba4e0e1fc1a200c54095e6d57b5540e9
Arfie <arfienade7@gmail.com>
2025-06-21 15:14:49 +0700

    Delete Simple Problem Study Materials 5 directory

--- Simple Problem Study Materials 5/problem5.py
+++
@@ -1,9 +0,0 @@
-# perform arithmatic operation on two number
-
-a = int(input("Enter one  number"))
-b = int(input("Enter 2nd number"))
-print("pertambahan = "+str(a+b))
-print("pengurangan = "+str(a-b))
-print("perkalian = "+str(a*b))
-print("pembagian = "+str(a/b))
-print("desimal = "+str(a%b))
